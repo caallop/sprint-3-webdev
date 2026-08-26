18f9e5b6a228f3ac3e13309f298610adf59b907b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-26 19:33:40 -0300

    merge

---
+++ sprint-3/app/login/page.jsx
@@ -0,0 +1,31 @@
+"use client";
+import Navbar from "../components/Nav";
+
+export default function LoginPage() {
+    return (
+        <div className="">
+            <Navbar />
+
+            <main className="">
+                <div className="login container">
+                    <h1 className="titulo-login">Login - Raluguva</h1>
+
+                    <form className="entrada">
+                        <div>
+                            <label className="">Email</label>
+                            <input type="email" className="" />
+                        </div>
+                        <div>
+                            <label className="">Senha</label>
+                            <input type="password" className="" />
+                        </div>
+
+                        <button className="">
+                            Entrar
+                        </button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    );
+}
